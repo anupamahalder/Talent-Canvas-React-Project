60fd976a6dc3bea27fbfe6b48d0fc3a3702f894c
Anupama_Halder <anupamahalder2022@gmail.com>
2023-11-19 17:44:54 +0530

    Added job card with loading data and changed button color:

--- src/Components/JobCard.jsx
+++ src/Components/JobCard.jsx
@@ -4,7 +4,7 @@ const JobCard = ({job}) => {
     // destructure job object 
     const {jobBannerImageUrl,jobTitle,loggedInUserName,jobCategory,category_key,salaryRange,jobDescription,jobPostingDate,applicationDeadline,jobApplicantsNumber} = job;
     return (
-        <div>
+        <div className='pb-10'>
             <div className="relative flex flex-col text-gray-700 bg-white shadow-md w-96 rounded-xl bg-clip-border">
             <div className="relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg rounded-xl bg-blue-gray-500 bg-clip-border shadow-blue-gray-500/40">
                 <img
@@ -13,7 +13,7 @@ const JobCard = ({job}) => {
             </div>
             <div className="p-6">
                 <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-                UI/UX Review Check
+                {jobTitle}
                 </h5>
                 <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
                 The place is close to Barceloneta Beach and bus stop just 2 min by walk
@@ -23,11 +23,11 @@ const JobCard = ({job}) => {
             </div>
             <div className="p-6 pt-3">
                 <button
-                className="block w-full select-none rounded-lg bg-pink-500 py-3.5 px-7 text-center align-middle font-sans text-sm font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                className="block w-1/2 mx-auto select-none rounded-lg bg-[#FF6348] py-3.5 px-7 text-center align-middle font-sans text-sm font-bold uppercase text-white shadow-md shadow-[#FF6348]/20 transition-all hover:shadow-lg hover:shadow-[#9a3624]/60 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                 type="button"
                 data-ripple-light="true"
                 >
-                Reserve
+                See Details
                 </button>
             </div>
             </div>
