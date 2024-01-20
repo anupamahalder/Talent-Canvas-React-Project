12d71198ad14dc6a82d2570026109b2cc58a695b
Anupama_Halder <anupamahalder2022@gmail.com>
2024-01-21 00:02:16 +0530

    Added three buttons for sorting all jobs table

--- src/Pages/AllJobs/AllJobs.jsx
+++ src/Pages/AllJobs/AllJobs.jsx
@@ -9,27 +9,30 @@ const AllJobs = () => {
     // declare a state to store jobs data 
     const [jobs, setJobs] = useState(loadedJobs);
     return (
-        <div className="p-10 mx-auto">
+        <>
+        <div className="pt-10 mx-auto">
             {/* heading  */}
-            <h1 className="mx-auto text-center font-bold uppercase text-2xl text-blue-800 text-shadow">See All Jobs</h1>
-            <div className="md:flex bg-gray-100 p-2">
+            <h1 className="mx-auto text-center font-bold uppercase text-2xl text-blue-800 drop-shadow-xl">See All Jobs</h1>
+            <div className="md:flex md:gap-2 p-2">
                 <div className="md:flex-1 flex gap-2 justify-start">
                     <button className="btn bg-[#3ba51d] text-white hover:bg-[#216b17]">See Recent Post</button>
                     <button className="btn bg-[#348ae6] text-white hover:bg-[#20339e]">See Old Post</button>
+                    <button className="btn bg-[#e58e26] text-white hover:bg-[#cc8e35]">See All Post</button>
                 </div>
-                <div className="md:flex-1 flex justify-end">
+                <div className="md:flex-1 pt-2 md:pt-0 flex justify-center md:justify-end">
                     <div className="">
-                        <input className="py-3 px-1 md:px-2 rounded-md outline outline-1 mr-2"
+                        <input className="py-[10px] px-1 md:px-2 rounded-md outline outline-1 mr-2 lg:w-[300px]"
                         type="text" placeholder="Search job by title"/>
                         <button className="btn bg-[#e64f34] text-white hover:bg-[#b64632]">Search</button>
                     </div>
                 </div>
             </div>
-            <div className="overflow-x-auto">
+            </div>
+            <div className="overflow-x-auto px-4 md:px-10">
             <table className="table">
                 {/* head */}
                 <thead>
-                <tr className="text-2xl bg-gray-500 text-base-200">
+                <tr className="text-xl bg-gray-300 text-gray-600">
                     <th>Name</th>
                     <th>Job Title</th>
                     <th>Job Posting Date</th>
@@ -43,7 +46,7 @@ const AllJobs = () => {
                 }
             </table>
             </div>
-        </div>
+        </>
     );
 };
 
