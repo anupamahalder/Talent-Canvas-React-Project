7ce572147110427dcc48c581430924b66d22a428
Anupama_Halder <anupamahalder2022@gmail.com>
2024-01-24 14:58:27 +0530

    Added add job form and fetch data from the form

--- src/Pages/AddJobs/AddJobs.jsx
+++ src/Pages/AddJobs/AddJobs.jsx
@@ -1,33 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useAuth from '../../Hooks/useAuth';
 
 const AddJobs = () => {
-
+    
+    // destructure authcontext values 
+    const {user} = useAuth();
+    // const {_id,jobBannerImageUrl,jobTitle,loggedInUserName,jobCategory,category_key,salaryRange,jobDescription,jobPostingDate,applicationDeadline,jobApplicantsNumber} = job;
     // handle add jobs 
     const hanldeAddJob = (e) =>{
         e.preventDefault();
         const form = e.target;
-
+        const jobTitle = form.jobTitle.value;
+        const jobBannerImageUrl = form.bannerUrl.value;
+        const loggedInUserName = user.displayName;
+        const userEmail = user.email;
+        const category_key = form.jobCategory.value;
+        const jobCategory = form.jobCategory.options[form.jobCategory.selectedIndex].getAttribute('fullname');
+        const highestSalary = form.highestSalary.value;
+        const lowestSalary = form.lowestSalary.value;
+        const jobDescription = form.jobDescription.value;
+        const appliedDate = new Date().toISOString().split('T')[0];
+        const applicationDeadline = form.applicationDeadline.value;
+        const applicantNumber = 0;
+        const salaryRange = `$${lowestSalary}-$${highestSalary}`;
+        const jobDetails = {jobTitle,jobBannerImageUrl,loggedInUserName, userEmail,jobCategory,category_key,salaryRange,jobDescription,appliedDate,applicationDeadline,applicantNumber};
+        console.log(jobDetails);
     }
     return (
-        <div className='min-h-screen pt-20 px-10 flex justify-center mb-20'>
-                <div className='bg-[#fdf1ee] text-[#555555] w-[600px] p-4 shadow-2xl rounded-lg text-xl'>
-                <h1 className="mx-auto text-center font-bold uppercase text-2xl text-blue-800 drop-shadow-xl my-6">Add Your Job</h1>
+        <div className='min-h-screen py-20 px-10 flex justify-center mb-20'>
+                <div className='bg-[#fbebe6] text-[#555555] w-[600px] p-4 shadow-2xl rounded-lg text-xl'>
+                <h1 className="mx-auto text-center font-bold uppercase text-3xl text-blue-800 drop-shadow-xl my-6">Add Your Job</h1>
                     <form onSubmit={hanldeAddJob}  >
+                        {/* job title  */}
                         <span className='uppercase pl-4 font-semibold text-gray-700 mr-3'>Job Title:</span>
-                        <input className='outline outline-1 mt-2 hover:outline-2 outline-slate-300 hover:outline-gray-800 rounded-md w-3/5' type="text" name="jobTitle" id="" /> <br /><br />
-
+                        <input required className='outline pl-2 outline-1 mt-2 hover:outline-2 outline-slate-300 hover:outline-gray-800 rounded-md w-3/5' type="text" name="jobTitle" id="" /> <br /><br />
+                        {/* picture url  */}
                         <span className='uppercase pl-4 font-semibold text-gray-700'>Picture URL of the Job Banner: </span>
-                        <input className='ml-6 outline mt-2 outline-1 outline-slate-300 rounded-lg w-3/4 hover:outline-2 hover:outline-gray-800' type="text" name="banner" id="" /><br /><br />
+                        <input required className='ml-6 pl-2 outline mt-2 outline-1 outline-slate-300 rounded-lg w-4/5 hover:outline-2 hover:outline-gray-800' type="text" name="bannerUrl" id="" /><br /><br />
+                        {/* select job category  */}
+                        <span className='uppercase pl-4 font-semibold text-gray-700'>Choose Job Category: </span>
 
+                        <select name="jobCategory" className='bg-white py-1 w-1/3 ml-2 pl-2 outline mt-2 outline-1 outline-slate-300 rounded-lg hover:outline-2 hover:outline-gray-800' id="jobCategory">
+                        <option value="fulltime" fullname="Full Time Job" defaultChecked>Full Time Job</option>
+                        <option value="parttime" fullname="Part Time Job">Part Time Job</option>
+                        <option value="remote" fullname="Remote Job">Remote Job</option>
+                        <option value="hybrid" fullname="Hybrid Job">Hybrid Job</option>
+                        <option value="onsite" fullname="On Site Job">On Site Job</option>
+                        <option value="intern" fullname="Intern Job">Intern Job</option>
+                        </select> <br /><br />
+
+                        {/* salary range  */}
                         <span className='uppercase pl-4 font-semibold text-gray-700'>Salary Range:</span><br />
-                        <input type="number" className='mt-2 w-40 px-2 mr-10 ml-6 outline outline-1 outline-slate-300 rounded-lg hover:outline-2 hover:outline-gray-800' name='lowestSalary' placeholder='lowest salary' />
-                        <input type="number" className='mt-2 w-40 px-2 outline outline-1 outline-slate-300 rounded-lg hover:outline-2 hover:outline-gray-800' name='highestSalary' placeholder='highest salary' /> <br /><br />
-
+                        <input required type="number" className='mt-2 w-40 px-2 mr-10 ml-6 outline outline-1 outline-slate-300 rounded-lg hover:outline-2 hover:outline-gray-800' name='lowestSalary' placeholder='lowest salary' />
+                        <input required type="number" className='mt-2 w-40 px-2 outline outline-1 outline-slate-300 rounded-lg hover:outline-2 hover:outline-gray-800' name='highestSalary' placeholder='highest salary' /> <br /><br />
+                        {/* job description  */}
                         <span className='uppercase pl-4 font-semibold text-gray-700'>Job Description:</span><br />
-                        <textarea name="jobDescription" className='outline outline-1 outline-slate-300 rounded-lg mt-2 w-5/6 mx-6 hover:outline-2 hover:outline-gray-800' id="" cols="10" rows="5"></textarea><br /><br />
-
+                        <textarea required name="jobDescription" className='outline outline-1 pl-2 outline-slate-300 rounded-lg mt-2 w-5/6 mx-6 hover:outline-2 hover:outline-gray-800' id="" cols="10" rows="5"></textarea><br /><br />
+                        {/* date input  */}
                         <span className='uppercase pl-4 font-semibold text-gray-700'>Application Deadline:  </span>
-                        <input className='outline outline-1 outline-slate-300 rounded-lg hover:outline-2 hover:outline-gray-800' type="date" placeholder='select date' name="applicationDeadline" id="" />
+                        <input required
+                         className='outline outline-1 px-2 outline-slate-300 rounded-lg hover:outline-2 hover:outline-gray-800' type="date" placeholder='select date' name="applicationDeadline" 
+                         min={new Date().toISOString().split('T')[0]}
+                         id="customeDate" /> <br />
+                         <div className='flex justify-center my-10'>
+                            <input type="reset" className='btn mr-2 text-xl bg-gray-500 text-white hover:bg-white hover:text-gray-800' value="Reset Form" />
+                         <input type="submit" className='btn text-2xl bg-[#FF6348] text-gray-700 hover:text-white hover:bg-red-700' value="Submit Job" />
+                         </div>
                     </form>
                 </div>
         </div>
