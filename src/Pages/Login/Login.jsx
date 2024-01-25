509fc24a49d11dad5862ace55fa20ee23997d965
Anupama_Halder <anupamahalder2022@gmail.com>
2024-01-25 10:59:57 +0530

    Added illustrator to login and register page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -78,8 +78,11 @@ const Login = () => {
 
     const labelStyle = "before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-800 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-800 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-[#f3591d] peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-[#f17c4d] peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-[#f17c4d] peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-800";
     return (
-        <div className="flex justify-center items-center h-screen">
-            <div className="relative flex flex-col text-gray-700 bg-white shadow-lg w-96 rounded-xl bg-clip-border">
+        <div className="md:flex justify-center items-center mx-auto min-h-screen py-20 lg:px-10">
+            <div className="-mb-1 flex justify-center mx-auto w-3/5 md:w-1/2">
+                <img src="https://blush.design/api/download?shareUri=io9vDIdwZEjiNCae&c=Hair_0%7Ed5e1d5-0.2%7Eff0048_Rainbow_0%7E008bf7-0.2%7E7ffc51_Skin_0%7Eb02d1c-0.2%7Efeb1cd&w=800&h=800&fm=png" alt="" />
+            </div>
+            <div className="relative m-4 lg:mr-10 flex flex-col text-gray-700 bg-white shadow-lg w-80 md:w-96 rounded-xl bg-clip-border">
                 <div className="relative grid mx-4 mb-4 -mt-6 overflow-hidden text-white shadow-lg h-28 place-items-center rounded-xl bg-gradient-to-tr from-[#f3591d] to-[#ffbca2] bg-clip-border shadow-[#FEA47F]/40">
                 <h3 className="block font-sans text-3xl antialiased font-semibold leading-snug tracking-normal text-white">
                     Sign In
