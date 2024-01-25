2a95fdfe3476d24c9a70ffe49c5fc65b9305d2a6
Anupama_Halder <anupamahalder2022@gmail.com>
2024-01-26 00:31:45 +0530

    Add the heading for responsiveness

--- src/Pages/MyJobs/MyJobCard.jsx
+++ src/Pages/MyJobs/MyJobCard.jsx
@@ -1,12 +1,16 @@
 import PropType from 'prop-types';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const MyJobCard = ({job}) => {
     // destructure job object 
     const {_id,jobBannerImageUrl,jobTitle,loggedInUserName,jobCategory,category_key,salaryRange,jobDescription,jobPostingDate,applicationDeadline,jobApplicantsNumber} = job;
+    // declare a state 
+    const [updateData, setUpdateData] = useState([]);
     // handle update button 
     const handleUpdateBtn = () =>{
         console.log('update btn is clicked!', _id);
+
     }
     return (
         <div className='pb-10'>
@@ -27,6 +31,7 @@ const MyJobCard = ({job}) => {
                     className="mx-auto select-none rounded-lg bg-[#ce2323] hover:bg-[#561c12] py-3.5 px-8 text-center align-middle font-sans text-sm font-bold uppercase text-white shadow-md shadow-[#915146]/20 transition-all hover:shadow-lg hover:shadow-[#472923]/60 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                     type="button"
                     data-ripple-light="true">Delete</button>
+                <Link to=''>
                 <button onClick={handleUpdateBtn}
                 className="mx-auto select-none rounded-lg bg-[#3b73e4] hover:bg-[#1a2460] py-3.5 px-8 text-center align-middle font-sans text-sm font-bold uppercase text-white shadow-md shadow-[#915146]/20 transition-all hover:shadow-lg hover:shadow-[#472923]/60 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                 type="button"
@@ -34,6 +39,7 @@ const MyJobCard = ({job}) => {
                 >
                 Update
                 </button>
+                </Link>
             </div>
             </div>
         </div>
